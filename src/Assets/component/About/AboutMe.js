e151be65dfb52b08983048084c065276be29fdf7
CamilaRovirosa <camilarovirosaochoa@gmail.com>
2022-11-11 17:21:55 -0600

    About responsivo

--- src/Assets/component/About/AboutMe.js
+++ src/Assets/component/About/AboutMe.js
@@ -2,18 +2,32 @@ import './About.scss';
 //import Banner from "../../component/Banner/Banner";
 import circulo from "../../images/CirculoRosa.png"
 import love from '../../images/LovePortrait.png';
-import Card from './Card.js';
-const AboutMe = ()=> {
-    return (
-       
-        <section className='shape'>
-            <div className='title'>
-                <h1>Un Poco Sobre Mi</h1>
-            </div>
-            <Card/>
+import Card from './Card'
+
+const AboutMe =() =>{
+    return(
+        
+        <section className='shape'>         
+                <div className='title'>
+                    <h1>Un Poco Sobre Mi</h1>
+                </div>
+                <div className='slider'>                   
+                        <div className='slider__pic'>
+                            <img src={love} alt=""/>
+                                                       
+                        </div>
+                        <div className="slider__content">
+                            <div className="slider__box">
+                                <div className="slider__title">En Escencia</div>
+                                <div className="slider__text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                </div>
+                            </div>
+                        </div>                  
+                </div >    
         </section>
-        
-        
     );
 }
 export default AboutMe;
--- src/Assets/component/Skills/Skills.js
+++ src/Assets/component/Skills/Skills.js
@@ -3,23 +3,18 @@ import './Skills.scss';
 import love from '../../images/LovePortrait.png';
 const Skills =() =>{
     return(
-        <section className='shape'>
-            <div className="container">
+        <section className='shape'>         
                 <div className='title'>
                     <h1>Mis Habilidades</h1>
                 </div>
-                <div className='slider-grid'>
-                    
-                    <div className='card'>
-                        <div className='slider-potrait'>
-                        <img src={love} alt=""/>
+                <div className='slider-grid'>                   
+                        <div className='slider-grid__pic'>
+                            <img src={love} alt=""/>
                         </div>
-                        <div className="slide-content">
-
-                        </div>
-                    </div>
+                        <div className="slider-grid__content">
+                            <h3>Core</h3>
+                        </div>                  
                 </div >    
-            </div>  
         </section>
     );
 }
