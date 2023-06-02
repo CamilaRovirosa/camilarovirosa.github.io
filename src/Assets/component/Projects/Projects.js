93593d0be60c7dd44a5fabf26ae99fb4050398ed
CamilaRovirosa <camilarovirosaochoa@gmail.com>
2023-06-01 22:14:24 -0600

    Seccion de proyectos quedo la estructura

--- src/Assets/component/Projects/Projects.js
+++ src/Assets/component/Projects/Projects.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './Projects.scss';
 import {SiCodefactor, SiExercism} from "react-icons/si"
-import {AiOutlineCheckCircle, AiOutlineRightSquare} from "react-icons/ai"
 import { IoGameController } from "react-icons/io5";
+import Pro1 from  './../../images/Parallax/Compressed/casa1-5_compressed.png';
 const Projects = ()=> {
     return (
         <section name="projects" className='pro'>         
@@ -15,6 +15,59 @@ const Projects = ()=> {
                     <SiExercism/>
                     <span> </span>
                     <IoGameController />
+                </div>
+                <div className="top3">
+                {/* //////////////proyecto 1//////////////*/}  
+                    <div className="top3-project">
+                        <img src={Pro1}
+                            alt="Proyecto del protafolio"
+                            className="top3-poster">
+                        </img>
+                        <div className="top3-title">Portafolio :)</div>
+                        <div className="top3-info">
+                            <span className="top3-comment">Por que me gusto mucho como quedo este portafolio.</span>
+                        </div>
+                        <div className="top3-desc">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Mauris commodo nisi eu magna maximus, eget rhoncus diam dictum. 
+                            Aenean vulputate commodo fringilla. 
+                            Suspendisse tortor magna, bibendum nec lobortis in, consectetur id magna. Nam ut dolor at sapien blandit semper eu ac ipsum. Sed nec tempor massa, vel sodales ipsum. Proin tristique sapien eu odio finibus, nec suscipit turpis ornare. Proin euismod cursus magna, ut semper diam eleifend non. Phasellus cursus massa arcu, sit amet rutrum magna ullamcorper sed. 
+                        </div>
+                    </div>
+                {/* //////////////proyecto 2//////////////*/}  
+                    <div className="top3-project">
+                        <img src={Pro1}
+                            alt="Proyecto del protafolio"
+                            className="top3-poster">
+                        </img>
+                        <div className="top3-title">Portafolio :^</div>
+                        <div className="top3-info">
+                            <span className="top3-comment">Por que me gusto mucho como quedo este portafolio.</span>
+                        </div>
+                        <div className="top3-desc">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Mauris commodo nisi eu magna maximus, eget rhoncus diam dictum. 
+                            Aenean vulputate commodo fringilla. 
+                            Suspendisse tortor magna, bibendum nec lobortis in, consectetur id magna. Nam ut dolor at sapien blandit semper eu ac ipsum. Sed nec tempor massa, vel sodales ipsum. Proin tristique sapien eu odio finibus, nec suscipit turpis ornare. Proin euismod cursus magna, ut semper diam eleifend non. Phasellus cursus massa arcu, sit amet rutrum magna ullamcorper sed. 
+                        </div>
+                    </div>
+                {/* //////////////proyecto 3//////////////*/}
+                    <div className="top3-project">
+                        <img src={Pro1}
+                            alt="Proyecto del protafolio"
+                            className="top3-poster">
+                        </img>
+                        <div className="top3-title">Portafolio :^</div>
+                        <div className="top3-info">
+                            <span className="top3-comment">Por que me gusto mucho como quedo este portafolio.</span>
+                        </div>
+                        <div className="top3-desc">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Mauris commodo nisi eu magna maximus, eget rhoncus diam dictum. 
+                            Aenean vulputate commodo fringilla. 
+                            Suspendisse tortor magna, bibendum nec lobortis in, consectetur id magna. Nam ut dolor at sapien blandit semper eu ac ipsum. Sed nec tempor massa, vel sodales ipsum. Proin tristique sapien eu odio finibus, nec suscipit turpis ornare. Proin euismod cursus magna, ut semper diam eleifend non. Phasellus cursus massa arcu, sit amet rutrum magna ullamcorper sed. 
+                        </div>
+                    </div>
                 </div>
         </section>
     );
