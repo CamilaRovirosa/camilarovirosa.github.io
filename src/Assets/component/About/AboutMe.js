8c6f5af8863060187f05b0d8ec1f649c376e76e2
CamilaRovirosa <camilarovirosaochoa@gmail.com>
2024-08-13 20:22:40 +0000

    merge conflicts de navbar y aboutme

--- src/Assets/component/About/AboutMe.js
+++ src/Assets/component/About/AboutMe.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './About.scss';
 import React , { useState } from 'react';
 import { useTranslation } from "react-i18next";
@@ -109,4 +110,86 @@ const AboutMe =() =>{
         </section>
     );
 }
+=======
+import './About.scss';
+import React , { useState } from 'react';
+import { useTranslation } from "react-i18next";
+
+import { GiThink, GiAxolotl, GiHeartOrgan, GiBrain } from "react-icons/gi";
+import { FaLaptopCode} from "react-icons/fa";
+import { TiArrowLeftOutline, TiArrowRightOutline } from "react-icons/ti";
+import { IoGameController } from "react-icons/io5";
+
+import circulo from "../../images/CirculoRosa.png"
+import love from '../../images/LovePortrait.png';
+import cuadrado from '../../images/cuadroazul.png';
+import core from '../../images/CorePortrait.png';
+import verde from '../../images/CuadroVerde.png';
+import work from '../../images/WorkPortrait.png';
+
+const AboutMe =() =>{
+    const {t} = useTranslation();
+    
+
+    const [current, setCurrent] = useState(0);
+    const length = t('slides').length;
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+
+
+
+    return( 
+        <section name='about' className='about'>  
+            <div className='about-container'>             
+                <div className='title'>
+                    <h2 id="h2">{t('aboutTitle')}</h2>
+                </div>
+                <div className='title__arrA'>
+                    <GiHeartOrgan/>
+                    <span> </span>
+                    <IoGameController/>
+                    <span> </span>
+                    <FaLaptopCode/>
+                        
+                </div>
+                <div className='slider ' >  
+                    <div className='slider__item'>
+                                        
+                        <div className='slider__pic'>
+                            <div className="slider__pic--holder"  onClick={nextSlide}>
+                                <img src={slides[current].image} className={slides[current].style} alt={slides[current].id}/>
+                                <img src={slides[current].image2} className={slides[current].style1} alt={slides[current].id}/>
+                            </div>                                
+                        </div>
+                        <div className="slider__content">
+                            
+                                <div className="slider__title space">{slides[current].title}
+                                <span className=" space">{slides[current].icon} </span>
+                                </div>
+                                <div className="slider__text">{slides[current].description}</div>
+                                <div className='arrow'>
+                                
+                                
+                                    <TiArrowLeftOutline className='slider__content--arrow ' onClick={nextSlide}/>
+                                    
+                                    <TiArrowRightOutline className='slider__content--arrow'onClick={prevSlide}/>
+                                    
+                                </div>
+                            
+                        </div>  
+                        
+                    </div>
+                    
+                </div >
+            </div>
+        </section>
+    );
+}
+>>>>>>> parent of ff71444 (new buil)
 export default AboutMe;
--- src/Assets/component/Nav/NavBar.js
+++ src/Assets/component/Nav/NavBar.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, {  useState }  from "react";
 import {Link} from 'react-scroll';
 import "./NavBar.scss";
@@ -63,4 +64,71 @@ const NavBar = () =>{
         
     );
 }
+=======
+import React, {  useState }  from "react";
+import {Link} from 'react-scroll';
+import "./NavBar.scss";
+import Logo from '../../images/favicon/favicon-96x96.png';
+import CV from '../../Documents/CV_CamilaRovirosa_Español.pdf';
+import  {GiHamburgerMenu} from "react-icons/gi"
+
+
+const NavBar = () =>{
+   const [showNav, setShowNav] = useState(false);
+   const handleShowNav=()=>{
+        setShowNav(!showNav);
+   }
+   onResumeClick() {
+    window.open(CV);
+  }
+  
+    return ( 
+        //Desktop
+        <div className="nav-bar-container">
+            {/*Desktop*/}
+                <ul className="left">
+                    <li className="logo nav-bar-tab left">
+                        <Link  to='home'>
+                            <img src = {Logo} className='ico' alt="logo"/>
+                        </Link>
+                    </li>
+                </ul>
+                <ul className={showNav ? 'movil-show' : 'right'} >
+                    <li className="nav-bar-tab">
+                        <Link  to="about" smooth={true} duration={500}  >
+                            SOBRE MI                      
+                        </Link>
+                    </li>
+                    <li className="nav-bar-tab">
+                        <Link  to="skills" smooth={true} duration={500}>
+                            HABILIDADES                         
+                        </Link>
+                    </li>
+                    <li className="nav-bar-tab">
+                        <Link  to="projects" smooth={true} duration={500}>
+                            PROYECTOS        
+                        </Link>
+                    </li>
+                    <li className="nav-bar-tab" >
+                        <Link  to="contact" smooth={true} duration={500}>
+                            CONTACTO
+                        </Link>
+                    </li>
+                    <li className="nav-bar-tab">
+                    <button onClick={this.onResumeClick}>CV</button>
+                    </li>
+                </ul>
+            {/*/Mobile*/} 
+            <span className="menu"
+                onClick={handleShowNav}>
+                    <GiHamburgerMenu/>
+            </span>
+            
+        </div>
+       
+            
+        
+    );
+}
+>>>>>>> parent of ff71444 (new buil)
 export default NavBar;
